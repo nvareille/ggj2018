dd277e320565bd72ac11145e66eb4b6bc4bc3a63
Nicolas VAREILLE <nicolas.vareille@epitech.eu>
2018-01-27 23:57:17 +0100

    Add life bar

--- Assets/Scripts/Character/HeroStats.cs
+++ Assets/Scripts/Character/HeroStats.cs
@@ -1,7 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class HeroStats : AStats {
+public class HeroStats : AStats
+{
+    public Slider LifeBar;
 
+    public void Update()
+    {
+        LifeBar.value = ((float)CurrentLife / (float)Life);
+    }
 }
