86e0b5e8b95594c1528fc7c1eb98cba2517d9f93
Nicolas VAREILLE <nicolas.vareille@epitech.eu>
2018-01-28 01:03:39 +0100

    Add traits gui

--- Assets/Scripts/Character/HeroStats.cs
+++ Assets/Scripts/Character/HeroStats.cs
@@ -6,12 +6,40 @@ using UnityEngine.UI;
 public class HeroStats : AStats
 {
     public Slider LifeBar;
-    
 
+    public Image[] BonusGUI;
+    public Image[] MalusGUI;
+
+    public TraitScriptableObject[] Bonus;
+    public TraitScriptableObject[] Malus;
 
     public void Awake()
     {
-        
+        int count;
+
+        count = 0;
+        foreach (TraitScriptableObject o in Bonus)
+        {
+            BonusGUI[count].gameObject.SetActive(true);
+            BonusGUI[count].sprite = o.Sprite;
+            Life += o.Life;
+            Damage += o.Damage;
+            Speed += o.Speed;
+            AtkSpeed += o.AtkSpeed;
+            ++count;
+        }
+
+        count = 0;
+        foreach (TraitScriptableObject o in Malus)
+        {
+            MalusGUI[count].gameObject.SetActive(true);
+            MalusGUI[count].sprite = o.Sprite;
+            Life += o.Life;
+            Damage += o.Damage;
+            Speed += o.Speed;
+            AtkSpeed += o.AtkSpeed;
+            ++count;
+        }
     }
 
     public void Update()
