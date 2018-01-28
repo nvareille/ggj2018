3e5b78534977705f55c45a888a29411ef88589fe
Nicolas VAREILLE <nicolas.vareille@epitech.eu>
2018-01-28 13:57:49 +0100

    Add sound Triggers & sounds

---
+++ Assets/Scripts/SoundTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class SoundTrigger : MonoBehaviour
+    {
+        public AudioClip Clip;
+
+        private AudioSource Source;
+
+        public void Awake()
+        {
+            Source = FindObjectOfType<Camera>().GetComponent<AudioSource>();
+        }
+
+        public void OnTriggerEnter(Collider collider)
+        {
+            if (collider.gameObject.layer == LayerMask.NameToLayer("Character") && Source.clip != Clip)
+            {
+                Source.clip = Clip;
+                Source.Play();
+            }
+        }
+    }
+}
